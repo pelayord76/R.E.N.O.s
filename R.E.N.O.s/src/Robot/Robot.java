e3381a2651ec3787bc6c9177bef94aa05a4841a1
Pelayo <pelayord76@educastur.es>
2022-12-22 13:16:56 +0100

    ROBOT 3

--- R.E.N.O.s/src/Robot/Robot.java
+++ R.E.N.O.s/src/Robot/Robot.java
@@ -1,5 +1,7 @@
 package Robot;
 
+import java.util.concurrent.TimeUnit;
+
 public class Robot {
 
 	private String nombre;
@@ -15,8 +17,109 @@ public class Robot {
 
 		this.nombre = nombre;
 		this.puntosDeVida = 100;
+		// el ataque tendra un valor entre 0 y 20.
 		this.puntosDeAtaque = (int) (Math.random() * 21);
+		// la defensa tendra un valor entre 0 y 100.
 		this.puntosDeDefensa = (int) (Math.random() * 101);
+	}
+
+	/**
+	 * metodo que hace que dos robots peleen entre ellos, en cada turno sacarán un
+	 * numero aleatorio entre 0 y 100, si este supera el valor defensivo del rival,
+	 * conseguirá lanzar el ataque causándole tanto daño como puntos de ataque
+	 * tenga.
+	 * 
+	 * @param otro: contrincante contra el que se pegara el primer robot.
+	 * @throws InterruptedException habrá una pausa de 2 segundos entre accion y
+	 *                              accion para que se pueda seguir el transcurso de
+	 *                              la pelea a una velocidad razonable.
+	 */
+	public void lucha(Robot otro) throws InterruptedException {
+
+		System.out.println("Va a comenzar la pelea entre " + this.nombre + " y " + otro.nombre + "!!");
+
+		// el bucle se repetira hasta que uno de los dos robots se quede sin vida.
+		while (this.puntosDeVida > 0 && otro.puntosDeVida > 0) {
+
+			int numeroA = (int) (Math.random() * 101);
+
+			// si el numero aleatorio supera la defensa rival comienza el proceso de ataque.
+			if (numeroA > otro.puntosDeDefensa && otro.puntosDeVida > 0 && this.puntosDeVida>0) {
+
+				TimeUnit.SECONDS.sleep(2);
+
+				// en caso de que los puntos de ataque sean mayores a la vida restante, se le
+				// restará solo la vida que le queda al robot.
+				if (otro.puntosDeVida < this.puntosDeAtaque) {
+
+					this.puntosDeAtaque = otro.puntosDeVida;
+				}
+
+				System.out.println(this.nombre + " le ha quitado " + this.puntosDeAtaque + " puntos de vida a "
+						+ otro.nombre + "!");
+
+				otro.puntosDeVida -= this.puntosDeAtaque;
+
+				TimeUnit.SECONDS.sleep(2);
+
+				System.out.println("Vida restante de " + otro.nombre + ": " + otro.puntosDeVida + ".");
+			}
+
+			// en caso de que el numero no supere la defensa, no atacará y aparte se sacara
+			// un mensaje por pantalla que avise del fallo.
+			if (numeroA <= otro.puntosDeDefensa && otro.puntosDeVida > 0 && this.puntosDeVida>0) {
+
+				TimeUnit.SECONDS.sleep(2);
+
+				System.out.println(otro.nombre + " ha evitado el ataque de " + this.nombre + "!!");
+
+			}
+
+			// se hace lo mismo con el caso del otro robot.
+			int numeroB = (int) (Math.random() * 101);
+
+			if (numeroB > this.puntosDeDefensa) {
+
+				TimeUnit.SECONDS.sleep(2);
+
+				if (this.puntosDeVida < otro.puntosDeAtaque) {
+
+					otro.puntosDeAtaque = this.puntosDeVida;
+				}
+
+				System.out.println(otro.nombre + " le ha quitado " + otro.puntosDeAtaque + " puntos de vida a "
+						+ this.nombre + "!");
+
+				this.puntosDeVida -= otro.puntosDeAtaque;
+
+				TimeUnit.SECONDS.sleep(2);
+
+				System.out.println("Vida restante de " + this.nombre + ": " + this.puntosDeVida + ".");
+			}
+
+			if (numeroB <= this.puntosDeDefensa && this.puntosDeVida > 0) {
+
+				TimeUnit.SECONDS.sleep(2);
+
+				System.out.println(this.nombre + " ha evitado el ataque de " + otro.nombre + "!!");
+			}
+		}
+
+		// cuando la vida de uno de los dos se ponga a 0, saldrá un mensaje por pantalla
+		// que anuncie al ganador.
+		if (this.puntosDeVida <= 0) {
+			
+			TimeUnit.SECONDS.sleep(2);
+
+			System.out.println(this.nombre + " se ha debilitado!! " + otro.nombre + " se lleva la victoria!!");
+		}
+
+		if (otro.puntosDeVida <= 0) {
+			
+			TimeUnit.SECONDS.sleep(2);
+
+			System.out.println(otro.nombre + " se ha debilitado!! " + this.nombre + " se lleva la victoria!!");
+		}
 	}
 
 	// getters y setters
--- R.E.N.O.s/src/Robot/Start.java
+++ R.E.N.O.s/src/Robot/Start.java
@@ -1,33 +1,16 @@
 package Robot;
 
-// import java.util.concurrent.TimeUnit;
-
 public class Start {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		/*
-		
-		// FORMA DE HACER QUE EL PROGRAMA "DUERMA" EN MEDIO DE LA EJECUCION: (descomentar java.utils para usar)
-		
-		// bucle infinito:
-		while (true) {
-			
-			// orden para que pare, unidad (que elijo segundos) y la cantidad de segundos entre accion y accion:
-			TimeUnit.SECONDS.sleep(1);
-			
-			// accion a ejecutar:
-			System.out.println("Hola");
-		}
-		
-		*/
-		
 		Robot a = new Robot("Atom");
 		Robot b = new Robot("Zeus");
-		
+
 		System.out.println(a);
 		System.out.println(b);
-
+		
+		a.lucha(b);
 	}
 
 }
